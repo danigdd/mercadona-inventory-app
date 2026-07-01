c4cd4125e890da9ab01aa846012904d5f09f5921
whath <danigenov@protonmail.com>
2026-07-01 14:56:20 +0200

    feat: add  validation for categories

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -1,4 +1,16 @@
 const db = require("../db/queries");
+const {body, validationResult, matchedData} = require("express-validator");
+
+const alphaErr = "debe contener solo letras";
+const lengthErr = "debe estar entre 1 y 15 carácteres";
+
+const validateUser = [
+  body("category_name").trim()
+  .isAlpha()
+  .withMessage(`El nombre de la categoría ${alphaErr}`)
+  .isLength({min: 1, max: 10})
+  .withMessage(`El nombre de la categoría ${lengthErr}`),
+];
 
 async function categoriesGet(req, res) {
   const categories = await db.getAllCategories();
@@ -17,10 +29,19 @@ function categoriesGetNew(req, res) {
 }
 
 //post created category
-async function categoriesPostNew(req, res){
+categoriesPostNew = [validateUser, async (req, res) => {
+  const errors = validationResult(req);
+
+  if(!errors.isEmpty()) {
+    return res.status(400).render("newCategory", {
+      errors: errors.array()
+    });
+  }
+
   const name = await req.body["category_name"];
   db.createCategory(name);
   res.redirect("/categorias");
-
 }
+];
+
 module.exports = {categoriesDelete, categoriesGet, categoriesGetNew, categoriesPostNew};
